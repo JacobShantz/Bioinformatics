cac7a6887474f0d96e980aa15a6d150e9ae66b9e
JacobShantz <jacob.k.shantz@wmich.edu>
2025-01-30 17:25:42 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,6 @@
+bash
+echo Hello Drew!
+echo This script is helping me to learn bash.
+mkdir -p bioinformatics/bash_scripts
+mv /Users/jacobshantz/Downloads/bash_template.txt /Users/jacobshantz/bioinformatics/bash_scripts
+mv /Users/jacobshantz/bioinformatics/bash_scripts/bash_template.txt /Users/jacobshantz/bioinformatics/bash_scripts/bash_template.sh
